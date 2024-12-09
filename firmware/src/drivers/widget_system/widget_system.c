06e95251ea68c13b3cca58d61bd5ed266ca0c181
Griffin Boyle <griffinboyle@leaflabs.com>
2024-12-09 14:41:04 -0500

    Correct system clock setup to use MSI and not HSI (was actually using MSI anyways since HSI wasn't properly configured anyways)

--- firmware/src/drivers/widget_system/widget_system.c
+++ firmware/src/drivers/widget_system/widget_system.c
@@ -36,7 +36,7 @@ static void widget_system_clock_init(void) {
     rcc_clk_init.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK |
                               RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
 
-    rcc_clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+    rcc_clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
     rcc_clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
     rcc_clk_init.APB1CLKDivider = RCC_HCLK_DIV1;
     rcc_clk_init.APB2CLKDivider = RCC_HCLK_DIV1;
